62332ba3dc6cfbdce9b7fc4ccd3277cd48969be9
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-06 23:06:26 +0530

    feat: add employee auth

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,6 +1,7 @@
-import type { Errback, Request, Response } from "express";
+import type { Request, Response } from "express";
 import { Customer } from "../models/customer";
 import bycrypt from "bcrypt";
+import { Employee } from "../models/employee";
 
 export const CustomerSignup = async (req: Request, res: Response) => {
   const { username, password } = req.body;
@@ -47,9 +48,45 @@ export const CustomerLogin = async (req: Request, res: Response) => {
 };
 
 export const EmployeeSignup = async (req: Request, res: Response) => {
-  return;
+  const { username, password } = req.body;
+
+  try {
+    // handle existing employee
+    const isExistingEmployee = await Employee.findOne({ username });
+    if (isExistingEmployee) {
+      return res.status(400).json({ error: "username already exists" });
+    }
+
+    // create new employee
+    const employee = new Employee({ username, password });
+    await employee.save();
+    const token = employee.generateAuthToken();
+    return res.status(200).json({ token });
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
 };
 
 export const EmployeeLogin = async (req: Request, res: Response) => {
-  return;
+  const { username, password } = req.body;
+
+  try {
+    // handle non-existing employee
+    const employee = await Employee.findOne({ username });
+    if (!employee) {
+      return res.status(400).json({ error: "invalid username or password" });
+    }
+
+    // check password
+    const passwordMatch = await bycrypt.compare(password, employee.password);
+    if (!passwordMatch) {
+      res.status(400).json({ error: "invalid username or password" });
+    }
+
+    // generate & return token
+    const token = employee.generateAuthToken();
+    return res.status(200).json({ token });
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
 };
