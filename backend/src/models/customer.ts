6e76eb2cc40d655d90a575fbfce28d06a4aa7412
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-06 21:35:11 +0530

    feat: add password hashing & token gen

--- backend/src/models/customer.ts
+++ backend/src/models/customer.ts
@@ -1,4 +1,6 @@
 import { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const Customer = new Schema({
   username: {
@@ -10,4 +12,36 @@ export const Customer = new Schema({
     type: String,
     required: true,
   },
+
+  passwordHash: {
+    type: String,
+    required: true,
+  },
 });
+
+// hash the password
+Customer.pre("save", function (next) {
+  const customer = this;
+
+  if (!customer.isModified("password")) {
+    return next();
+  }
+
+  bcrypt
+    .hash(customer.password, 10)
+    .then((hash) => {
+      customer.passwordHash = hash;
+      next();
+    })
+    .catch((err) => next(err));
+});
+
+// generate auth token
+Customer.methods.generateAuthToken = function () {
+  const customer = this;
+  const token = jwt.sign(
+    { _id: customer._id },
+    process.env.JWT_SECRET as string
+  );
+  return token;
+};
