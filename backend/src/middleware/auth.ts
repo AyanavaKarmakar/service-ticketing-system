b685455976df3e0878314eff7d104b169f4e7c2c
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-06 22:55:10 +0530

    feat: add middleware for employee login

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,8 +1,9 @@
 import jwt, { type JwtPayload } from "jsonwebtoken";
 import { Customer } from "../models/customer";
+import { Employee } from "../models/employee";
 import type { Request, Response, NextFunction } from "express";
 
-export const authMiddleware = async (
+export const customerAuthMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -21,6 +22,7 @@ export const authMiddleware = async (
     if (!decoded || typeof decoded !== "object" || !decoded._id) {
       throw new Error("Invalid token");
     }
+
     const customer = await Customer.findOne({
       _id: decoded._id,
     });
@@ -28,6 +30,40 @@ export const authMiddleware = async (
     if (!customer) throw new Error();
 
     req.customer = customer;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please authenticate" });
+  }
+};
+
+export const employeeAuthMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.header("Authorization");
+    if (!authHeader) throw new Error();
+
+    const token = authHeader.replace("Bearer ", "");
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    if (!decoded || typeof decoded !== "object" || !decoded._id) {
+      throw new Error("Invalid token");
+    }
+
+    const employee = await Employee.findOne({
+      _id: decoded._id,
+    });
+
+    if (!employee) throw new Error();
+
+    req.employee = employee;
     req.token = token;
     next();
   } catch (e) {
--- backend/src/types/custom.d.ts
+++ backend/src/types/custom.d.ts
@@ -1,8 +1,10 @@
 import { ICustomer } from "../models/customer";
+import { IEmployee } from "../models/employee";
 
 declare global {
   namespace Express {
     interface Request {
+      employee?: IEmployee;
       customer?: ICustomer;
       token?: string;
     }
