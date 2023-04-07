82b6ba1eb59a30bee9d3ab5e24c8f376bdcb9672
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-07 18:26:05 +0530

    feat: add middleware to check if emp is admin

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -5,6 +5,7 @@ import { Customer } from "../models/customer";
 import { Employee } from "../models/employee";
 import type { Request, Response, NextFunction } from "express";
 
+// middleware to check if the customer is authenticated
 export const customerAuthMiddleware = async (
   req: Request,
   res: Response,
@@ -39,6 +40,7 @@ export const customerAuthMiddleware = async (
   }
 };
 
+// middleware to check if the employee is authenticated
 export const employeeAuthMiddleware = async (
   req: Request,
   res: Response,
@@ -70,5 +72,40 @@ export const employeeAuthMiddleware = async (
     next();
   } catch (e) {
     res.status(401).send({ error: "Please authenticate" });
+  }
+};
+
+// middleware to check if the employee is authenticated and is an admin
+export const adminAuthMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.header("Authorization");
+    if (!authHeader) throw new Error();
+
+    const token = authHeader.replace("Bearer ", "");
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    if (!decoded || typeof decoded !== "object" || !decoded._id) {
+      throw new Error("Invalid token");
+    }
+
+    const employee = await Employee.findOne({
+      _id: decoded._id,
+    });
+
+    if (!employee || employee.username !== "employee1") throw new Error();
+
+    req.employee = employee;
+    req.token = token;
+    next();
+  } catch (e) {
+    return res.status(403).json({ error: "Forbidden" });
   }
 };
