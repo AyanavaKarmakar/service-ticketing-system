a99c1baf0ad2054d792d319948dad756708c4f65
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-08 20:23:54 +0530

    feat: add base footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import { Navbar, AuthForm } from "./components";
 
 const App = () => {
-  console.log(
-    localStorage.getItem("token"),
-    localStorage.getItem("username"),
-    localStorage.getItem("userType")
-  );
+  console.log(localStorage.getItem("token"));
 
   return (
-    <>
-      <Navbar />
+    <main>
+      <nav>
+        <Navbar />
+      </nav>
       <AuthForm />
-    </>
+
+      <footer>
+        <p className="text-center text-base text-black font-medium">
+          Logged in as username
+        </p>
+      </footer>
+    </main>
   );
 };
 
