a322616f7f01ba87563b6340e378f5404590c78d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-08 21:30:01 +0530

    feat: setup react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+
+import "./index.css";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </QueryClientProvider>
   </React.StrictMode>
