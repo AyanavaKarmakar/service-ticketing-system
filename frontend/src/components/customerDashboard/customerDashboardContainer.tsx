8d8a7e48899c7461178e0c852051b1cf6ac242d0
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-09 17:12:50 +0530

    feat: add button for creating new request

--- frontend/src/components/customerDashboard/customerDashboardContainer.tsx
+++ frontend/src/components/customerDashboard/customerDashboardContainer.tsx
@@ -1,17 +1,24 @@
 import { useNavigate } from "react-router-dom";
+import { clsx } from "clsx";
 
 export const CustomerDashboardContainer = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <h1>Dashboard</h1>
+    <div className="flex flex-col gap-3 items-center justify-center">
+      <div className="text-center p-5 text-lg lg:text-3xl text-black font-bold underline">
+        Customer Dashboard
+      </div>
 
-      <br />
-
-      <button onClick={() => navigate("/dashboard/customer/requestForm")}>
-        go to form
+      <button
+        className={clsx(
+          "mb-3 py-2.5 px-5 bg-gray-900 text-xl text-white font-semibold rounded-md",
+          "focus:outline-none focus-visible:ring focus-visible:ring-gray-700 focus-visible:ring-opacity-75"
+        )}
+        onClick={() => navigate("/dashboard/customer/requestForm")}
+      >
+        Create Request
       </button>
-    </>
+    </div>
   );
 };
