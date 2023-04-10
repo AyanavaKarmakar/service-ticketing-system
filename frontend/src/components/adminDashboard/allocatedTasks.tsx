1505637f51d6069ed16f579003fba77d12d9b4c2
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-10 18:20:48 +0530

    feat: add query for allocated tasks

--- frontend/src/components/adminDashboard/allocatedTasks.tsx
+++ frontend/src/components/adminDashboard/allocatedTasks.tsx
@@ -1,8 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
 import { clsx } from "clsx";
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export const AllocatedTasks = () => {
   const navigate = useNavigate();
+
+  const getTasks = useQuery({
+    queryKey: ["getAllocatedTasks"],
+
+    queryFn: async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/tasks/allocated`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        toast.success("Allocated tasks fetched successfully!");
+      } else {
+        toast.error("Allocated tasks fetch failed!");
+      }
+
+      const data = await response.json();
+      return data.allocatedTasks;
+    },
+
+    enabled: true,
+
+    onError: () => {
+      toast.error("Interval server error. Please try again!");
+    },
+  });
+
+  console.log(getTasks.data);
 
   return (
     <button
