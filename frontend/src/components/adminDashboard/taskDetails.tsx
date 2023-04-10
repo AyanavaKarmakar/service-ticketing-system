1ee0553958bf752cb9e338821b15d164993c7bf6
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-10 18:47:32 +0530

    feat: fetch task by id

--- frontend/src/components/adminDashboard/taskDetails.tsx
+++ frontend/src/components/adminDashboard/taskDetails.tsx
@@ -1,8 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
 import { clsx } from "clsx";
-import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const TaskDetails = () => {
   const navigate = useNavigate();
+  const { requestFormId } = useLocation().state;
+
+  const getTaskDetails = useQuery({
+    queryKey: ["requestForm"],
+
+    enabled: true,
+
+    queryFn: async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/tasks/${requestFormId}/details`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        toast.success("Requested task fetched successfully!");
+      } else {
+        toast.error("Requested task fetch failed!");
+      }
+
+      const data = await response.json();
+      return data.task;
+    },
+
+    onError: () => {
+      toast.error("Interval server error. Please try again!");
+    },
+  });
+
+  console.log(getTaskDetails.data);
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
