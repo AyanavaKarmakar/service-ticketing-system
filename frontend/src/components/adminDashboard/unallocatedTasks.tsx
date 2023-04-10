b6e6039cee9d7948af67a4b1b167e6f81656a4b8
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-10 18:23:02 +0530

    feat: setup query for unallocated tasks

--- frontend/src/components/adminDashboard/unallocatedTasks.tsx
+++ frontend/src/components/adminDashboard/unallocatedTasks.tsx
@@ -1,8 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
 import { clsx } from "clsx";
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export const UnallocatedTasks = () => {
   const navigate = useNavigate();
+
+  const getTasks = useQuery({
+    queryKey: ["getUnallocatedTasks"],
+
+    queryFn: async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/tasks/unallocated`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        toast.success("Unallocated tasks fetched successfully!");
+      } else {
+        toast.error("Unallocated tasks fetch failed!");
+      }
+
+      const data = await response.json();
+      return data.unallocatedTasks;
+    },
+
+    enabled: true,
+
+    onError: () => {
+      toast.error("Interval server error. Please try again!");
+    },
+  });
+
+  console.log(getTasks.data);
 
   return (
     <button
