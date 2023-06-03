2c9683982bd1ed54e7f822dad0d094385b823982
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-03 16:43:51 +0530

    fix component create test for auth

--- angular-client/src/app/pages/auth/auth/auth.component.spec.ts
+++ angular-client/src/app/pages/auth/auth/auth.component.spec.ts
@@ -2,14 +2,33 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [AuthComponent],
-    });
+      imports: [
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
