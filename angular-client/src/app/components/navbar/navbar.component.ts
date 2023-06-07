ce76102ea35783d4d70ddc092a99e3f677bec42e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-07 18:01:31 +0530

    fix(angular-client): navbar router link

--- angular-client/src/app/components/navbar/navbar.component.ts
+++ angular-client/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    public userService: UserService
+  ) {}
 }
