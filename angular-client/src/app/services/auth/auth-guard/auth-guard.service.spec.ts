6300abce6a4d36c2165f0036a20ce67097a6bcf6
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-07 19:23:19 +0530

    feat(angular-client): add tests for authguard

--- angular-client/src/app/services/auth/auth-guard/auth-guard.service.spec.ts
+++ angular-client/src/app/services/auth/auth-guard/auth-guard.service.spec.ts
@@ -1,9 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthGuardService } from './auth-guard.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from '../auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 describe('AuthGuardService', () => {
   let service: AuthGuardService;
@@ -22,5 +22,25 @@ describe('AuthGuardService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should allow access if user is authenticated', () => {
+    const authService = TestBed.inject(AuthService);
+    spyOn(authService, 'isAuthenticated').and.returnValue(true);
+
+    const canActivate = service.canActivate();
+    expect(canActivate).toBeTrue();
+  });
+
+  it('should navigate to auth page if user is not authenticated', () => {
+    const authService = TestBed.inject(AuthService);
+    spyOn(authService, 'isAuthenticated').and.returnValue(false);
+
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
+
+    service.canActivate();
+
+    expect(router.navigate).toHaveBeenCalledWith(['/auth']);
   });
 });
