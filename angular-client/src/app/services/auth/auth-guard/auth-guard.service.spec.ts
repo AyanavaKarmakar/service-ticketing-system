9be088daf1f89dea5faafe1021d9b22578e5ee94
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-11 00:14:00 +0530

    add more tests

--- angular-client/src/app/services/auth/auth-guard/auth-guard.service.spec.ts
+++ angular-client/src/app/services/auth/auth-guard/auth-guard.service.spec.ts
@@ -40,4 +40,51 @@ describe('AuthGuardService', () => {
     expect(routerSpy).toHaveBeenCalledWith(['/auth']);
     expect(result).toBe(false);
   });
+
+  it('should allow access if user is authenticated and URL starts with correct user type', () => {
+    const routerSpy = spyOn(service['router'], 'navigate');
+
+    const isAuthenticatedSpy = spyOn(
+      service['authService'],
+      'isAuthenticated'
+    ).and.returnValue(true);
+
+    const getUserTypeSpy = spyOn(
+      service['userService'],
+      'getUserType'
+    ).and.returnValue('admin');
+
+    const route = new ActivatedRouteSnapshot();
+    const state = { url: '/admin/mock-url' } as RouterStateSnapshot;
+    const result = service.canActivate(route, state);
+
+    expect(isAuthenticatedSpy).toHaveBeenCalled();
+    expect(getUserTypeSpy).toHaveBeenCalled();
+    expect(routerSpy).not.toHaveBeenCalled();
+    expect(result).toBe(true);
+  });
+
+  it('should navigate to /{userType}/home if user is authenticated but URL does not start with correct user type', () => {
+    const routerSpy = spyOn(service['router'], 'navigate');
+
+    const isAuthenticatedSpy = spyOn(
+      service['authService'],
+      'isAuthenticated'
+    ).and.returnValue(true);
+
+    const getUserTypeSpy = spyOn(
+      service['userService'],
+      'getUserType'
+    ).and.returnValue('admin');
+
+    const route = new ActivatedRouteSnapshot();
+    const state = { url: '/customer/home' } as RouterStateSnapshot;
+    const result = service.canActivate(route, state);
+
+    expect(isAuthenticatedSpy).toHaveBeenCalled();
+    expect(getUserTypeSpy).toHaveBeenCalled();
+    expect(routerSpy).toHaveBeenCalledWith(['/admin/home']);
+    // we return true but because the router has already navigated before returning
+    expect(result).toBe(true);
+  });
 });
