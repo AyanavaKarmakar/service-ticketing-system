0f51f04baf70d187b3e0054ba8b393d9305c8e42
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-10 23:47:20 +0530

    add route redirects

--- angular-client/src/app/services/auth/auth-guard/auth-guard.service.ts
+++ angular-client/src/app/services/auth/auth-guard/auth-guard.service.ts
@@ -6,25 +6,32 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { UserService } from '../../user/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   canActivate(
+    // throws error without route : ActivatedRouteSnapshot parameter
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
     const isAuthenticated = this.authService.isAuthenticated();
+    const userType = this.userService.getUserType();
 
     if (!isAuthenticated) {
       this.router.navigate(['/auth']);
     }
 
-    if (isAuthenticated && !state.url.startsWith('/customer')) {
-      this.router.navigate(['/customer/home']);
+    if (isAuthenticated && !state.url.startsWith(`/${userType}`)) {
+      this.router.navigate([`/${userType}/home`]);
     }
 
     return isAuthenticated;
