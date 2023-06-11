65cbf946e958d6f531eb014b49865c87790ea3c2
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-11 18:37:46 +0530

    fix tests

--- angular-client/src/app/pages/home/home/customer/customer/customer.component.spec.ts
+++ angular-client/src/app/pages/home/home/customer/customer/customer.component.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomerComponent } from './customer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('CustomerComponent', () => {
   let component: CustomerComponent;
   let fixture: ComponentFixture<CustomerComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CustomerComponent],
+      imports: [MatIconModule],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CustomerComponent]
-    });
     fixture = TestBed.createComponent(CustomerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
