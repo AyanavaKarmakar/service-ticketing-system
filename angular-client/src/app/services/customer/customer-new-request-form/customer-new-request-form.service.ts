bfce16f2ccfcb07b7c7579372a3e62d4a4ee4b1c
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-13 23:39:07 +0530

    base

--- angular-client/src/app/services/customer/customer-new-request-form/customer-new-request-form.service.ts
+++ angular-client/src/app/services/customer/customer-new-request-form/customer-new-request-form.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserService } from '../../user/user.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerNewRequestFormService {
+  constructor(private http: HttpClient, private userService: UserService) {}
 
-  constructor() { }
+  createNewFormRequest() {
+    const authToken = this.userService.getAuthToken();
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`,
+      }),
+    };
+
+    this.http.post<any>(environment.customerRequestUrl, httpOptions);
+  }
 }
