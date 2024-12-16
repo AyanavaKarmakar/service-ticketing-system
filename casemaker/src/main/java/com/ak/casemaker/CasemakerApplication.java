0d912263065f59905630cec30041acc4d0501f1e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-12-16 18:50:15 +0530

    chore: setup Java BE

---
+++ casemaker/src/main/java/com/ak/casemaker/CasemakerApplication.java
@@ -0,0 +1,13 @@
+package com.ak.casemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasemakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasemakerApplication.class, args);
+	}
+
+}
---
+++ casemaker/src/test/java/com/ak/casemaker/CasemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.casemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
