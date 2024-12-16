62302592e9b9c29f63243098aae9a5a6b3c4da65
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-12-16 19:08:01 +0530

    feat: add GlobalExceptionHandler.java

---
+++ casemaker/src/main/java/com/ak/casemaker/libs/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ak.casemaker.libs.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getReason());
+        return new ResponseEntity<>(response, ex.getStatusCode());
+    }
+}
