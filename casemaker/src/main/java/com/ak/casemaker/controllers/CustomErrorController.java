e92f274aa1a672bc599f109b7a76d36e9cb46694
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-12-16 18:59:52 +0530

    feat: disable default whitelabel error & add a CustomErrorController

---
+++ casemaker/src/main/java/com/ak/casemaker/controllers/CustomErrorController.java
@@ -0,0 +1,24 @@
+package com.ak.casemaker.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public ResponseEntity<Map<String, Object>> handleError() {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+        errorResponse.put("error", "Not Found");
+        errorResponse.put("message", "Something went wrong, OR you're lost...");
+        errorResponse.put("path", "/error");
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
