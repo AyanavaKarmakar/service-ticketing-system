ee813e6f6a60069d0413929eca3e80d705716695
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-12-16 19:00:21 +0530

    chore: remove unused import

--- casemaker/src/main/java/com/ak/casemaker/controllers/CustomErrorController.java
+++ casemaker/src/main/java/com/ak/casemaker/controllers/CustomErrorController.java
@@ -5,8 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.util.HashMap;
 import java.util.Map;
 
