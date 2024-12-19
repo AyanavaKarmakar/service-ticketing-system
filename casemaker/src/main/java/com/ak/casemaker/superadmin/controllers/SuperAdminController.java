4cb79ba1fe307d8c9d2c5d283e7c094223484f66
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-12-19 18:38:40 +0530

    feat: add DELETE endpoint for Super Admin

--- casemaker/src/main/java/com/ak/casemaker/superadmin/controllers/SuperAdminController.java
+++ casemaker/src/main/java/com/ak/casemaker/superadmin/controllers/SuperAdminController.java
@@ -1,6 +1,6 @@
 package com.ak.casemaker.superadmin.controllers;
 
-import com.ak.casemaker.libs.utils.CustomApiResponse;
+import com.ak.casemaker.libs.utils.ResponseWrapper;
 import com.ak.casemaker.superadmin.models.SuperAdmin;
 import com.ak.casemaker.superadmin.services.SuperAdminServiceInterface;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,22 +21,32 @@ public class SuperAdminController {
 
     @Operation(summary = "Get a Super Admin", description = "Fetch a Super Admin by their ID")
     @GetMapping("{id}")
-    public ResponseEntity<CustomApiResponse<SuperAdmin>> getSuperAdmin(@PathVariable("id") Long id) {
+    public ResponseEntity<ResponseWrapper<SuperAdmin>> getSuperAdmin(@PathVariable("id") Long id) {
         SuperAdmin superAdmin = superAdminService.getSuperAdmin(id);
-        CustomApiResponse<SuperAdmin> customApiResponse = new CustomApiResponse<>(
+        ResponseWrapper<SuperAdmin> responseWrapper = new ResponseWrapper<>(
                 superAdmin,
                 "Super Admin with id: " + id + ", found");
-        return new ResponseEntity<>(customApiResponse, HttpStatus.FOUND);
+        return new ResponseEntity<>(responseWrapper, HttpStatus.FOUND);
     }
 
     @Operation(summary = "Get all Super Admins", description = "Fetch list of all Super Admins")
     @GetMapping("/all")
-    public ResponseEntity<CustomApiResponse<List<SuperAdmin>>> getAllSuperAdmins() {
+    public ResponseEntity<ResponseWrapper<List<SuperAdmin>>> getAllSuperAdmins() {
         List<SuperAdmin> superAdmins = superAdminService.getAllSuperAdmins();
-        CustomApiResponse<List<SuperAdmin>> customApiResponse = new CustomApiResponse<>(
+        ResponseWrapper<List<SuperAdmin>> responseWrapper = new ResponseWrapper<>(
                 superAdmins,
                 "List of all Super Admins"
         );
-        return new ResponseEntity<>(customApiResponse, HttpStatus.FOUND);
+        return new ResponseEntity<>(responseWrapper, HttpStatus.FOUND);
+    }
+
+    @Operation(summary = "Delete a Super Admin", description = "Delete a Super Admin by their ID")
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteSuperAdmin(Long id) {
+        superAdminService.deleteSuperAdmin(id);
+        return new ResponseEntity<>(
+                "Super Admin with ID: " + id + ", has been deleted successfully",
+                HttpStatus.OK
+        );
     }
 }
