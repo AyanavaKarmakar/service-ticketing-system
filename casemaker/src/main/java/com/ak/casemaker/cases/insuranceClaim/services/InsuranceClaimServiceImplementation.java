1462cfc894f779c6f39dff25a1d459cdbbe615cf
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2025-01-01 15:58:06 +0530

    feat: add iclaim service

--- casemaker/src/main/java/com/ak/casemaker/cases/insuranceClaim/services/InsuranceClaimServiceImplementation.java
+++ casemaker/src/main/java/com/ak/casemaker/cases/insuranceClaim/services/InsuranceClaimServiceImplementation.java
@@ -5,7 +5,9 @@ import com.ak.casemaker.cases.insuranceClaim.repository.InsuranceClaimRepository
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -17,16 +19,30 @@ public class InsuranceClaimServiceImplementation implements InsuranceClaimServic
 
     @Override
     public InsuranceClaim getInsuranceClaim(Long id) {
-        return null;
+        InsuranceClaim insuranceClaim = insuranceClaimRepository.findById(id).orElseThrow(() -> {
+            String errorMessage = "Insurance Claim with id: " + id + ", doesn't not exist";
+            LOGGER.error(errorMessage);
+            return new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
+        });
+
+        LOGGER.info("Insurance Claim with id: {}, found", id);
+        return insuranceClaim;
     }
 
     @Override
     public List<InsuranceClaim> getAllInsuranceClaims() {
-        return List.of();
+            LOGGER.info("Returning list of all Insurance Claims");
+            return insuranceClaimRepository.findAll();
     }
 
     @Override
     public void deleteInsuranceClaim(Long id) {
-
+        InsuranceClaim insuranceClaim = insuranceClaimRepository.findById(id).orElseThrow(() -> {
+            String errorMessage = "Insurance Claim with id: " + id + ", not found. Unable to delete Insurance Claim";
+            LOGGER.error(errorMessage);
+            return new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
+        });
+         insuranceClaimRepository.delete(insuranceClaim);
+        LOGGER.info("Insurance Claim with id: {}, deleted successfully", id);
     }
 }
