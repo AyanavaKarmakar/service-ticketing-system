ae8d46163daf9121f9cd343ddaadb2c134978427
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2025-01-02 19:10:22 +0530

    feat: add service for creating a new insurance claim

--- casemaker/src/main/java/com/ak/casemaker/cases/insuranceClaim/services/InsuranceClaimServiceImplementation.java
+++ casemaker/src/main/java/com/ak/casemaker/cases/insuranceClaim/services/InsuranceClaimServiceImplementation.java
@@ -1,5 +1,6 @@
 package com.ak.casemaker.cases.insuranceClaim.services;
 
+import com.ak.casemaker.cases.insuranceClaim.DTOs.createInsuranceClaimDTO;
 import com.ak.casemaker.cases.insuranceClaim.models.InsuranceClaim;
 import com.ak.casemaker.cases.insuranceClaim.repository.InsuranceClaimRepository;
 import org.apache.logging.log4j.LogManager;
@@ -44,5 +45,23 @@ public class InsuranceClaimServiceImplementation implements InsuranceClaimServic
         });
          insuranceClaimRepository.delete(insuranceClaim);
         LOGGER.info("Insurance Claim with id: {}, deleted successfully", id);
+    }
+
+    @Override
+    public InsuranceClaim createInsuranceClaim(createInsuranceClaimDTO newInsuranceClaim) {
+        if (newInsuranceClaim.getIssueType() == null || newInsuranceClaim.getIssueType().isEmpty()) {
+            String errorMessage = "Issue type must not be null or empty";
+            LOGGER.error(errorMessage);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage);
+        }
+
+        InsuranceClaim insuranceClaim = new InsuranceClaim();
+        insuranceClaim.setIssueType(newInsuranceClaim.getIssueType());
+        insuranceClaim.setIssueDescription(newInsuranceClaim.getIssueDescription());
+        insuranceClaim.setStatus(newInsuranceClaim.getStatus());
+        InsuranceClaim savedClaim = insuranceClaimRepository.save(insuranceClaim);
+
+        LOGGER.info("Insurance Claim created with id: {}", savedClaim.getId());
+        return savedClaim;
     }
 }
